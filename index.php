426680fc8e67138b370a2fd3007a661a4856ab00
Rees Retuta <rees.retuta@gmail.com>
2014-08-31 15:27:51 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>Title!</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!-- <link rel="stylesheet" href="/reset.css?v=1"> -->
+    <link rel="stylesheet" href="/style.css?v=1">
+    <link rel="stylesheet" href="/reset.css?v=1">
+    
+    <link rel="shortcut icon" href="/media/images/favicon.ico">
+    <script type="text/javascript" src="/media/js/libs/modernizr-1.7.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.13.1/TweenMax.min.js"></script>
+</head>
+
+
+<body>
+
+
+    <img class="vegas-background2" src="/bg.jpg" />
+    <div id="logo">
+        
+    </div>
+    
+    <!-- main content wrapper end -->
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="/media/js/plugins.js"></script>        
+    <script type="text/javascript" src="/media/js/site.js"></script>
+    <script type="text/javascript">
+        var bg = $('.vegas-background2');
+        var logo = $('#logo');
+        // TweenLite.to(bg, 2, {left:"542px", backgroundColor:"black", borderBottomColor:"#90e500", color:"white"});
+        var intro = new TimelineLite();
+        
+        intro.to(bg,5,{
+          // opacity: 0,
+          scale: 2, 
+          transformOrigin:"50% 50% 0",
+          // ease: 'easeInQuart',
+          ease: 'easeInCubic',
+          // onStart: function(){
+//             $("#questions").fadeOut(1000);
+//           }
+        })
+        .to(bg,1.5,{opacity:0},'-=0.5')
+        .to(logo,1,{
+            // scale: 15, 
+            opacity:0,
+            transformOrigin:"50% 50% 0",
+            ease: 'easeOutCubic'
+        },'=1');
+
+    </script>
+</body>
+</html>    
---
+++ vegas/README.md
@@ -0,0 +1,21 @@
+# Vegas – Fullscreen Backgrounds and Slideshows with jQuery.
+
+Vegas is a jQuery plugin to add beautiful fullscreen backgrounds to your webpages. You can even create amazing Slideshows.
+
+    $(function() {
+	    $.vegas('/img/background.jpg')('overlay');
+	}
+
+### Official website
+http://vegas.jaysalvat.com/
+
+### Real life demo
+http://vegas.jaysalvat.com/demo/
+
+### Documentation
+http://vegas.jaysalvat.com/documentation/
+
+## Contributing
+
+Please don't edit files in the `dist` subdirectory as they are generated via Grunt. You'll find source code in the `src` subdirectory!
+Regarding code style like indentation and whitespace, **follow the conventions you see used in the source already.**
