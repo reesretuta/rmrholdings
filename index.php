2e5fb0ca58bdd6435064325db67adc0ee6e3701e
Rees Retuta <rees.retuta@gmail.com>
2014-08-31 20:34:54 -0700

    style

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!doctype html>
-<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-
+<html class="no-js" lang="en" ng-app="rockyMountainApp">
 <head>
 
     <meta charset="UTF-8">
@@ -14,54 +10,106 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
     <!-- <link rel="stylesheet" href="/reset.css?v=1"> -->
-    <link rel="stylesheet" href="/style.css?v=1">
+    <link rel="stylesheet" href="/css/style.css?v=1">
     <link rel="stylesheet" href="/reset.css?v=1">
     
     <link rel="shortcut icon" href="/media/images/favicon.ico">
     <script type="text/javascript" src="/media/js/libs/modernizr-1.7.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.13.1/TweenMax.min.js"></script>
+    <script src="/bower_components/angular/angular.js"></script>
+    <script src="/bower_components/angular/angular-routes.js"></script>
 </head>
-
-
 <body>
-    
-
-
     <img class="vegas-background2" src="/bg.jpg" />
-    <div id="logo">
+    <div id="logo0"></div>
+    <div class="header-bar clearfix">
+                <div id="logo">
+                    Rocky Mountain Resources
+                </div>
+    </div>
+    <div id="wrap">
+        <div id="sidebar">
+            <div class="left-menu">
+                <ul>
+                    <li>
+                        <a class="current" href="/">Home</a> 
+                    </li>
+                    <li>
+                    <a href="#/about">About NGP</a></li>
+                    <li><a href="#/investments">Investments</a></li>
+                    <li><a href="#/events">Events &amp; Appearances</a></li>
+                    <li><a href="#/team">Our Team</a></li>
+                    <li><a href="#/news/">News</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="right-content">
+                <div ng-view>
         
+                </div>
+        </div>
     </div>
     
+    <div id="footer">
+                    <p>Rocky Mountain Resources | 9595 Wilshire Blvd, Suite 310
+Beverly Hills, CA 90212 | Tel. + 1 310.409.4113 | <a href="#tc" id="fancyBoxLink">Terms and Conditions </a></p>
+            </div>
     <!-- main content wrapper end -->
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="/media/js/plugins.js"></script>        
     <script type="text/javascript" src="/media/js/site.js"></script>
     <script type="text/javascript">
-        var bg = $('.vegas-background2');
-        var logo = $('#logo');
-        // TweenLite.to(bg, 2, {left:"542px", backgroundColor:"black", borderBottomColor:"#90e500", color:"white"});
-        var intro = new TimelineLite();
-        
-        intro.to(bg,5,{
-          // opacity: 0,
-          scale: 2, 
-          transformOrigin:"50% 50% 0",
-          // ease: 'easeInQuart',
-          ease: 'easeInCubic',
-          // onStart: function(){
-//             $("#questions").fadeOut(1000);
-//           }
-        })
-        .to(bg,1.5,{opacity:0},'-=0.5')
-        .to(logo,1,{
-            // scale: 15, 
-            opacity:0,
-            transformOrigin:"50% 50% 0",
-            ease: 'easeOutCubic'
-        },'=1');
+    $(function(){
+        $("#sidebar li").on('click',function(){
+            $("#sidebar li").removeClass('current');
+            $(this).addClass('current');
+            
+        });
 
+        
+        $("#sidebar li").not('.current').hover(function(){
+            $(this).toggleClass('hover');
+            $(this).css('background-color', '#241f79');
+            },function(){
+            $(this).toggleClass('hover');
+            $(this).css('background-color', 'transparent');
+          });
+    });
+    var app = angular.module('rockyMountainApp',['ngRoute']);
+    // app.use(express.static(__dirname + '/app'));
+    app.config(function($routeProvider){
+        $routeProvider.when('/',
+        {
+            controller: 'homepageController',
+            templateUrl: 'app/views/home.html'
+        })
+        .when('/about',{
+            controller: 'aboutController',
+            templateUrl: 'app/views/about.html'
+        })
+        .when('/investments',{
+            templateUrl: 'app/views/investments.html'
+        })
+        .when('/events',{
+            templateUrl: 'app/views/events.html'
+        })
+        .when('/team',{
+            templateUrl: 'app/views/team.html'
+        });
+        
+    });
+    
+    app.controller('homepageController', function($scope){
+        
+    });
+    
+    app.controller('aboutController', function($scope){
+        
+    });
+    
+    
     </script>
 </body>
 </html>    
